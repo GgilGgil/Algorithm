a51a48b0831230d8acb2053c253807474441a1ea
Park GilNam <knzzang7845@gmail.com>
2018-02-22 14:55:58 +0900

    solve me first commit

---
+++ solveMeFirst/solveMeFirst/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  solveMeFirst
+//
+//  Created by Park GilNam on 2018. 2. 22..
+//  Copyright © 2018년 ParkGilNam. All rights reserved.
+//
+
+import Foundation
+
+
+func solveMeFirst(a:Int, b:Int) {
+    print(a + b)
+}
+
+var a = Int(readLine()!)!
+var b = Int(readLine()!)!
+
+solveMeFirst(a:a, b:b)
