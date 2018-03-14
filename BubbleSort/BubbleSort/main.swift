143ec23f1e36a95b610b151989c1d98e789d84b8
Park GilNam <knzzang7845@gmail.com>
2018-03-14 12:35:03 +0900

    bubble sort commit

--- BubbleSort/BubbleSort/main.swift
+++ BubbleSort/BubbleSort/main.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 
-print("Hello, World!")
+func bubblesort(arr:[Int] ) {
+    
+    var array = arr
+    
+    print("before : \(array)")
+    
+    for _ in 0..<array.count {
+        for j in 0..<array.count - 1 {
+            if array[j] > array[j + 1] {
+                array.swapAt(j, j + 1)
+            }
+        }
+    }
+    
+    print("after : \(array)")
+}
 
+var array = [5, 1, 3, 8, 6, 2, 4, 7, 10, 9]
+
+bubblesort(arr: array)
