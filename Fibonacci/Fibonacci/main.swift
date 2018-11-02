7ed8e03813e5f0e3610c8d981d41970538d97ab6
박길남 <gilnam_park@genieworks.net>
2018-11-02 16:31:24 +0900

    1. 링크드리스트 값 존재 유무 확인하는 함수 추가

--- Fibonacci/Fibonacci/main.swift
+++ Fibonacci/Fibonacci/main.swift
@@ -8,7 +8,57 @@
 
 import Foundation
 
-class matrix {
+//class matrix {
+//    var data:[[Double]] = Array(repeating: Array(repeating: 0, count: 2), count: 2)
+//
+//    init() {
+//        self.data[0][0] = 1
+//        self.data[0][1] = 1
+//        self.data[1][0] = 1
+//        self.data[1][1] = 0
+//    }
+//}
+//
+//func multi(A:matrix, B:matrix) -> matrix {
+//    let result = matrix()
+//
+//    result.data[0][0] = (A.data[0][0] * B.data[0][0]) + (A.data[0][1] * B.data[1][0])
+//    result.data[0][1] = (A.data[0][0] * B.data[0][1]) + (A.data[0][1] * B.data[1][1])
+//    result.data[1][0] = (A.data[1][0] * B.data[0][0]) + (A.data[1][1] * B.data[1][0])
+//    result.data[1][1] = (A.data[1][0] * B.data[0][1]) + (A.data[1][1] * B.data[1][1])
+//
+//    return result
+//}
+//
+//func calcu(A:matrix, n:Int) -> matrix {
+//    var a = A
+//
+//    if n > 1 {
+//        a = calcu(A: a, n: n/2)
+//        a = multi(A: a, B: a)
+//
+//        if n % 2 == 1 {
+//            let B:matrix = matrix()
+//            a = multi(A: a, B: B)
+//        }
+//    }
+//
+//    return a
+//}
+//
+//func fibonacci(n:Int) -> Double {
+//    if n == 0 {
+//        return 0
+//    }
+//
+//    var A:matrix = matrix()
+//    A = calcu(A:A, n:n)
+//
+//    return A.data[0][1]
+//}
+//
+
+class Matrix {
     var data:[[Double]] = Array(repeating: Array(repeating: 0, count: 2), count: 2)
     
     init() {
@@ -19,18 +69,18 @@ class matrix {
     }
 }
 
-func multi(A:matrix, B:matrix) -> matrix {
-    let result = matrix()
+func multi(A:Matrix, B:Matrix) -> Matrix {
+    let result:Matrix = Matrix()
     
     result.data[0][0] = (A.data[0][0] * B.data[0][0]) + (A.data[0][1] * B.data[1][0])
-    result.data[0][1] = (A.data[0][0] * B.data[1][0]) + (A.data[0][1] * B.data[1][1])
+    result.data[0][1] = (A.data[0][0] * B.data[0][1]) + (A.data[0][1] * B.data[1][1])
     result.data[1][0] = (A.data[1][0] * B.data[0][0]) + (A.data[1][1] * B.data[1][0])
-    result.data[1][1] = (A.data[1][0] * B.data[1][0]) + (A.data[1][1] * B.data[1][1])
+    result.data[1][1] = (A.data[1][0] * B.data[0][1]) + (A.data[1][1] * B.data[1][1])
     
     return result
 }
 
-func calcu(A:matrix, n:Int) -> matrix {
+func calcu(A:Matrix, n:Int) -> Matrix {
     var a = A
     
     if n > 1 {
@@ -38,7 +88,7 @@ func calcu(A:matrix, n:Int) -> matrix {
         a = multi(A: a, B: a)
         
         if n % 2 == 1 {
-            let B:matrix = matrix()
+            let B = Matrix()
             a = multi(A: a, B: B)
         }
     }
@@ -51,11 +101,48 @@ func fibonacci(n:Int) -> Double {
         return 0
     }
     
-    var A:matrix = matrix()
-    A = calcu(A:A, n:n)
+    var A = Matrix()
+    A = calcu(A: A, n: n)
     
     return A.data[0][1]
 }
 
+print(fibonacci(n: 10))
 
-print(fibonacci(n: 100))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- LinkedList/LinkedList/main.swift
+++ LinkedList/LinkedList/main.swift
@@ -8,28 +8,98 @@
 
 import Foundation
 
-class node<T:Equatable> {
-    var value:T? = nil
-    var next:node? = nil
+//class node<T:Equatable> {
+//    var value:T? = nil
+//    var next:node? = nil
+//}
+//
+//class linkedList<T:Equatable> {
+//    var head = node<T>()
+//
+//    func insert(value:T) {
+//        if head.value == nil {
+//            head.value = value
+//        } else {
+//            var lastNode = head
+//
+//            while lastNode.next != nil {
+//                lastNode = lastNode.next!
+//            }
+//
+//            let newNode = node<T>()
+//            newNode.value = value
+//            lastNode.next = newNode
+//        }
+//    }
+//
+//    func remove(value:T) {
+//        if head.value == nil {
+//            return
+//        }
+//
+//        if head.value == value {
+//            if head.next == nil {
+//                head.value = nil
+//            } else {
+//                head = head.next!
+//            }
+//
+//            return
+//        }
+//
+//        var currentNode = head
+//        var previousNode = node<T>()
+//
+//        while currentNode.value != value && currentNode.next != nil {
+//            previousNode = currentNode
+//            currentNode = currentNode.next!
+//        }
+//
+//        if currentNode.value == value {
+//            if currentNode.next != nil {
+//                previousNode.next = currentNode.next
+//            } else {
+//                previousNode.next = nil
+//            }
+//        }
+//    }
+//
+//    func printAllKeys() {
+//        var currentNode:node! = head
+//
+//        print("---------------")
+//
+//        while currentNode != nil && currentNode.value != nil {
+//            print("The item is \(currentNode.value!)")
+//            currentNode = currentNode.next
+//        }
+//    }
+//}
+
+class Node<T:Equatable> {
+    var value:T?
+    var next:Node?
 }
 
-class linkedList<T:Equatable> {
-    var head = node<T>()
+class LinkedList<T:Equatable> {
+    var head:Node = Node<T>()
     
     func insert(value:T) {
         if head.value == nil {
             head.value = value
-        } else {
-            var lastNode = head
-            
-            while lastNode.next != nil {
-                lastNode = lastNode.next!
-            }
-            
-            let newNode = node<T>()
-            newNode.value = value
-            lastNode.next = newNode
+            return
         }
+        
+        var current = head
+        
+        while current.next != nil {
+            current = current.next!
+        }
+        
+        let new = Node<T>()
+        new.value = value
+        
+        current.next = new
     }
     
     func remove(value:T) {
@@ -38,40 +108,68 @@ class linkedList<T:Equatable> {
         }
         
         if head.value == value {
-            head = head.next!
-            return
-        }
-        
-        var currentNode = head
-        var previousNode = node<T>()
-        
-        while currentNode.value != value && currentNode.next != nil {
-            previousNode = currentNode
-            currentNode = currentNode.next!
-        }
-        
-        if currentNode.value == value {
-            if currentNode.next != nil {
-                previousNode.next = currentNode.next
+            if head.next != nil {
+                head = head.next!
             } else {
-                previousNode.next = nil
+                head.value = nil
+            }
+        }
+        
+        var current = head
+        var prev = Node<T>()
+        
+        while current.value != value && current.next != nil {
+            prev = current
+            current = current.next!
+        }
+        
+        if current.value == value {
+            if current.next != nil {
+                prev.next = current.next
+            } else {
+                prev.next = nil
             }
         }
     }
     
     func printAllKeys() {
-        var currentNode:node! = head
+        var current = head
         
-        print("---------------")
+        print("=========")
         
-        while currentNode != nil && currentNode.value != nil {
-            print("The item is \(currentNode.value!)")
-            currentNode = currentNode.next
+        while current.next != nil {
+            if let value = current.value {
+                print(value)
+            }
+            
+            current = current.next!
         }
+        
+        if let value = current.value {
+            print(value)
+        }
+    }
+    
+    func isValue(value:T) -> Bool {
+        var current = head
+        
+        while current.next != nil {
+            if current.value == value {
+                return true
+            }
+            
+            current = current.next!
+        }
+        
+        if current.value == value {
+            return true
+        }
+        
+        return false
     }
 }
 
-var myList = linkedList<Int>()
+var myList = LinkedList<Int>()
 myList.insert(value: 10)
 myList.insert(value: 20)
 myList.insert(value: 30)
@@ -82,3 +180,23 @@ myList.remove(value: 40)
 myList.printAllKeys()
 myList.insert(value: 100)
 myList.printAllKeys()
+print(myList.isValue(value: 20))
+print(myList.isValue(value: 100))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
